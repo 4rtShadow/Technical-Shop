9ea0106a96a9619192d162466825a4c5a64c765a
4rtShadow <artshadowofficial@yandex.ru>
2025-11-08 20:38:48 +0300

    Fixed bugs

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,10 @@
+from app.core.database import Base
+
 from app.models.user import User
 from app.models.product import Product
 from app.models.cart import CartItem
 from app.models.order import Order, OrderItem
 from app.models.ticket import Ticket
 
-__all__ = ["User", "Product", "CartItem", "Order", "OrderItem", "Ticket"]
+__all__ = ["Base", "User", "Product", "CartItem", "Order", "OrderItem", "Ticket"]
 

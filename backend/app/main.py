1b425afdb4d183166fc29c6ec0c7264675304cf4
4rtShadow <artshadowofficial@yandex.ru>
2025-11-10 10:52:02 +0300

    Fixed API Docs

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,6 @@ app = FastAPI(
     title="TechStore API",
     description="REST API for Electronics Store",
     version="1.0.0",
-    openapi_version="3.1.0",
     docs_url=None,  # Отключаем стандартную документацию
     redoc_url=None  # Отключаем стандартную документацию
 )
